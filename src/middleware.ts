786b1aabbca52faaa48acb308207316b0d3836e5
Abdulrahman-zazo <abdulrahmanzazo@outlook>
2025-08-08 18:06:43 +0300

    Adding bing file2

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,6 @@ export default createIntlMiddleware({
 export const config = {
   matcher: [
     // تخطى كل المسارات الداخلية وملفات الـ static
-    "/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest|robots.txt|sitemap.xml).*)",
+    "/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest|robots.txt|sitemap.xml|62b4f46620ee4878a10397a1c6120973.txt).*)",
   ],
 };
